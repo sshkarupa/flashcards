e29d5b1d10f623ea57797dcf9eb85d7d18862b1b
sshkarupa <s.shkarupa@gmail.com>
2015-03-20 15:42:07 +0000

    fixing bugs

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,11 +2,11 @@ class Card < ActiveRecord::Base
   validates :original_text, :translated_text, presence: true
   validate :original_text_is_different_from_translated
 
-  before_create do
-    self.review_date = Date.today + 3
-  end
-
   private
+
+    before_create do
+      self.review_date = Date.today + 3
+    end
 
     def original_text_is_different_from_translated
       if original_text.mb_chars.downcase == translated_text.mb_chars.downcase
