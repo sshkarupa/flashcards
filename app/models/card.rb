b66f5092498c01a02112cc3b087df58e2e0dc8f3
sshkarupa <s.shkarupa@gmail.com>
2015-03-20 14:09:19 +0000

    fixing bugs

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,18 +1,15 @@
 class Card < ActiveRecord::Base
-  before_validation :set_review_date
-
-  validates :original_text, :translated_text, :review_date, presence: true
+  validates :original_text, :translated_text, presence: true
   validate :original_text_is_different_from_translated
 
-  def original_text_is_different_from_translated
-    if original_text.mb_chars.downcase == translated_text.mb_chars.downcase
-       errors[:base] << "оригинал и перевод не могут совпадать"
-    end
+  before_create do
+    self.review_date = Date.today + 3
   end
 
   private
-
-    def set_review_date
-      self.review_date = Date.today + 3
+    def original_text_is_different_from_translated
+      if original_text.mb_chars.downcase == translated_text.mb_chars.downcase
+        errors[:base] << "оригинал и перевод не могут совпадать"
+      end
     end
 end
