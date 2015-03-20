f027eb5f388052f1a4665b6102a1abcdf1085840
sshkarupa <s.shkarupa@gmail.com>
2015-03-20 11:11:26 +0000

    fixing bugs

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,13 @@
 class CardsController < ApplicationController
+
+  before_action :find_card, only: [:update, :edit, :destroy]
+
   def index
     @card = Card.all
   end
 
   def new
     @card = Card.new
-    new_card
   end
 
   def create
@@ -14,32 +16,28 @@ class CardsController < ApplicationController
     if @card.save
       redirect_to cards_path
     else
-      new_card
       render 'new'
     end
   end
 
   def update
-    @card = Card.find(params[:id])
-
     if @card.update(card_params)
       redirect_to cards_path
     else
-      edit_card
-      render 'new'
+      render 'edit'
     end
   end
 
   def edit
-    @card = Card.find(params[:id])
-    edit_card
   end
 
   def destroy
-    @card = Card.find(params[:id])
     @card.destroy
-
     redirect_to cards_path
+  end
+
+  def find_card
+    @card = Card.find(params[:id])
   end
 
 private
@@ -47,15 +45,4 @@ private
     params.require(:card).permit(:original_text, :translated_text)
   end
 
-  def new_card
-    @btn = "btn-info"
-    @header = "card__heading card__heading_new"
-    @title = "Создание карточки"
-  end
-
-  def edit_card
-    @btn = "btn-warning"
-    @header = "card__heading card__heading_edit"
-    @title = "Редактирование карточки"
-  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,15 +1,14 @@
 class Card < ActiveRecord::Base
+  before_validation :set_review_date
 
   validates :original_text, :translated_text, :review_date, presence: true
   validate :the_same_text
 
   def the_same_text
-    if original_text.downcase == translated_text.downcase
+    if original_text.mb_chars.downcase == translated_text.mb_chars.downcase
        errors[:base] << "оригинал и перевод не могут совпадать"
     end
   end
-
-  before_validation :set_review_date
 
   private
     def set_review_date
